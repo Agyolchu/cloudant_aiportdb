c19b6a4eb3159b6ffa73f78d306ba8cb04c490a8
Agyolchu <agilyolchuyev@outlook.com>
2019-03-31 20:06:04 +0200

    Ready backend for UI

--- api/app.py
+++ api/app.py
@@ -1,6 +1,29 @@
 from cloudant_service import CloudantService
 from connection_config import connection_credentials
+from flask import Flask, jsonify, request
+from flask_restful import Resource, Api
 
+app = Flask(__name__)
+api = Api(app)
 cloudant = CloudantService(**connection_credentials)
-dd = cloudant.get_airport_data(lat_dn=0, lon_dn=0, lon_up=30, lat_up=5)
-print(dd)
+
+
+class HomeAPI(Resource):
+    def get(self):
+        return "Hello Airport"
+
+
+class AirportAPI(Resource):
+    def get(self):
+        arguments = request.args.to_dict()
+        dd = cloudant.get_airport_data(**arguments)
+        data = {'result':dd}
+        return jsonify(data)
+
+
+api.add_resource(HomeAPI, '/')
+api.add_resource(AirportAPI, '/airport/')
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
