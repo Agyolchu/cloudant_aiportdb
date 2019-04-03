8a8e842b1b282c6aee18218a6adb234625ab1c7f
Agyolchu <agilyolchuyev@outlook.com>
2019-04-03 14:51:36 +0200

    Updated controller

--- api/app.py
+++ api/app.py
@@ -11,6 +11,7 @@ cloudant = CloudantService(**connection_credentials)
 @app.route('/', methods=['GET', 'POST'])
 def home():
     if request.method == 'POST':
+        measured_distances = {}
         all_airports = cloudant.get_airport_data()
         new_required_params = {}
         required_parameters = request.form.to_dict()
@@ -21,8 +22,7 @@ def home():
             nearest_airports = nearest_airports.find_nearest_airports()
             airport_data_process = AirportDataProcessor(nearest_airports, new_required_params)
             measured_distances = airport_data_process.distance_processor()
-            return render_template('airports.html', items=measured_distances)
-
+        return render_template('airports.html', items=measured_distances)
     elif request.method == 'GET':
         return render_template('index.html')
 
